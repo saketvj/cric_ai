127cdcc79d45513de356d49ac50bc187350e8b9f
saketvj <saketvijay58@gmail.com>
2026-05-17 16:29:48 +0530

    adding src to path

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+SRC_DIR = ROOT_DIR / "src"
+
+sys.path.append(str(SRC_DIR))
+
+
 import streamlit as st
 from pathlib import Path
 from config.constants import DB_PATH
